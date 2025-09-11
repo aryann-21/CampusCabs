ba37f41bf05afb218ed52cedce7884ba44027f76
Aryan <aryan21sharmaa@gmail.com>
2025-09-11 10:59:05 +0530

    fixed timeout

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/campuscabs';
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  bufferMaxEntries: 0, // Disable mongoose buffering
+  bufferCommands: false, // Disable mongoose buffering
+})
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.error("MongoDB connection error: ", err));
 
